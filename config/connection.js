75e89d9f7b1bc88ad29211eef07895413c209621
Tressa Park <tressapark@gmail.com>
2020-05-20 17:43:21 -0500

    updated README

--- config/connection.js
+++ config/connection.js
@@ -8,21 +8,23 @@ if (process.env.JAWSDB_URL) {
 } else {
   connection = mysql.createConnection({
     host: "localhost",
-    // port: 3306,
     user: "root",
     password: "root1234",
     database: "burger_db"
   });
-}
+};
 
 //if err throw error
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
-//make connection
+connection.connect();
 module.exports = connection;
+
+// (function (err) {
+//   if (err) {
+//     console.error("error connecting: " + err.stack);
+//     return;
+//   }
+//   console.log("connected as id " + connection.threadId);
+// });
+
+// //make connection
+// module.exports = connection;
